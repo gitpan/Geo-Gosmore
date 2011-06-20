76cc35ee4ced1798a6263c3252f5b11cfe3f9dfc
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2011-06-20 15:13:58 +0000

    Import of AVAR/Geo-Gosmore-0.05 from CPAN.
    gitpan-cpan-distribution: Geo-Gosmore
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         AVAR/Geo-Gosmore-0.05.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

--- lib/Geo/Gosmore/Query.pm
+++ lib/Geo/Gosmore/Query.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Geo::Gosmore::Query::AUTHORITY = 'cpan:AVAR';
 }
 BEGIN {
-  $Geo::Gosmore::Query::VERSION = '0.04';
+  $Geo::Gosmore::Query::VERSION = '0.05';
 }
 use Any::Moose;
 use warnings FATAL => "all";
--- lib/Geo/Gosmore/Route.pm
+++ lib/Geo/Gosmore/Route.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Geo::Gosmore::Route::AUTHORITY = 'cpan:AVAR';
 }
 BEGIN {
-  $Geo::Gosmore::Route::VERSION = '0.04';
+  $Geo::Gosmore::Route::VERSION = '0.05';
 }
 use Any::Moose;
 use warnings FATAL => "all";
